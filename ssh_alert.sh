094efbb0980355edcb30e9b2bc406cbccc3a98d9
elvizhuy <elvizhuy@gmail.com>
2023-10-09 12:00:30 +0700

    .

--- ssh_alert.sh
+++ ssh_alert.sh
@@ -12,20 +12,22 @@ IP=$(echo $SSH_CLIENT | awk '{print $1}')
 PORT=$(echo $SSH_CLIENT | awk '{print $3}')
 HOSTNAME=$(hostname -f)
 IPADDR=$(hostname -I | awk '{print $1}')
+
 TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
-TEXT="$DATE_EXEC: ${USER} logged in to $HOSTNAME ($IPADDR) from $IP - $ORG - $CITY, $REGION, $COUNTRY on port $PORT"
 
+TEXT="$DATE_EXEC: ${USER} logged in to $HOSTNAME ($IPADDR) from $IP on port $PORT"
 
-if [ -n "$SSH_CLIENT" ]; then
-	curl http://ipinfo.io/$IP -s -o $TMPFILE
-	CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
-	REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
-	COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
-	ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
-	curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
-	rm $TMPFILE
+if [[ ${USER} != 'ucmea' ]]; then
+   curl -s -X POST --max-time $TIMEOUT $URL -d "chat_id=$USERID" -d "text=<b>$TEXT</b>" -d "parse_mode=HTML" > /dev/null
 fi
 
-if [[ ${USER} != 'ucmea']]; then
-	curl -s -X POST --max-time $TIMEOUT $URL -d "chat_id=$USERID" -d text="$TEXT" > /dev/null 
-fi
+# if [ -n "$SSH_CLIENT" ]; then
+# 	curl http://ipinfo.io/$IP -s -o $TMPFILE
+# 	CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
+# 	REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
+# 	COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
+# 	ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
+# 	curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+# 	rm $TMPFILE
+# fi
+
